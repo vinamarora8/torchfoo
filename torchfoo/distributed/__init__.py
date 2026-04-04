a12f3fe65461ff251e2e4aa3b0ade26cbbed13b3
Vinam Arora <vinam@seas.upenn.edu>
2026-04-03 21:49:33 -0400

    refactor: break down distributed.py in distributed/

---
+++ torchfoo/distributed/__init__.py
@@ -0,0 +1,2 @@
+from .distributed import *  # noqa: F401,F403
+from .collective import *  # noqa: F401,F403
--- torchfoo/distributed/distributed.py
+++ torchfoo/distributed/distributed.py
@@ -2,11 +2,6 @@ __all__ = [
     "is_distributed",
     "get_world_size",
     "get_rank",
-    "all_reduce_",
-    "all_reduce_sum_",
-    "all_equal",
-    "all_concat_jagged",
-    "all_concat",
     "rank_zero_only",
     "setup_distributed",
     "cleanup_distributed",
@@ -16,7 +11,6 @@ __all__ = [
 import logging
 
 import torch
-from torch import Tensor
 import torch.distributed as dist
 
 
@@ -51,90 +45,6 @@ def get_rank() -> int:
         return 0
 
     return dist.get_rank()
-
-
-def all_reduce_(x: Tensor) -> Tensor:
-    r"""Perform an in-place backprop-able "all reduce" operation
-
-    Args:
-        x: a contiguous Tensor on this device
-
-    Returns:
-        The input tensor, modified in-place with the all-reduced mean.
-    """
-    if not x.is_contiguous():
-        raise ValueError("input must be contiguous")
-
-    return _AllReduce.apply(x)
-
-
-def all_reduce_sum_(x: Tensor) -> Tensor:
-    r"""Perform an in-place backprop-able "all reduce sum" operation
-
-    Args:
-        x: a contiguous Tensor on this device
-
-    Returns:
-        The input tensor, modified in-place with the all-reduced sum.
-    """
-    if not x.is_contiguous():
-        raise ValueError("input must be contiguous")
-
-    return _AllReduceSum.apply(x)
-
-
-def all_equal(x: Tensor) -> bool:
-    r"""Check if x is the same on all GPUs
-
-    Args:
-        x: a contiguous Tensor on this device
-
-    Returns:
-        True if x is equal across all processes.
-    """
-    if not x.is_contiguous():
-        raise ValueError("input must be contiguous")
-
-    nworld = get_world_size()
-    if nworld == 1:
-        return True
-
-    tensor_list = [torch.zeros_like(x) for _ in range(nworld)]
-    dist.all_gather(tensor_list, x)
-
-    return all((val == x).all() for val in tensor_list)
-
-
-def all_concat_jagged(x: Tensor) -> Tensor:
-    r"""Concatenate a Tensor with varying dim 0 from multiple GPUs
-
-    Not differentiable.
-
-    Args:
-        x: a Tensor on this device
-
-    Returns:
-        Concatenated tensor from all processes along dim 0.
-    """
-    nworld = get_world_size()
-    if nworld == 1:
-        return x
-
-    output = [None for _ in range(nworld)]
-    dist.all_gather_object(output, x)
-    return torch.concat(output)  # type: ignore
-
-
-def all_concat(x: Tensor) -> Tensor:
-    r"""Concatenate a Tensor distributed on multiple GPUs. Backprop-able.
-
-    Args:
-        x: a Tensor on this device
-
-    Returns:
-        Concatenated tensor from all processes along dim 0.
-    """
-    return _AllConcat.apply(x)
 
 
 def rank_zero_only(func):
@@ -304,57 +214,3 @@ def parallelize(
         return wrapper
 
     return decorator
-
-
-# Inspired by:
-# https://github.com/facebookresearch/ijepa/blob/52c1ae95d05f743e000e8f10a1f3a79b10cff048/src/utils/distributed.py
-class _AllConcat(torch.autograd.Function):
-
-    @staticmethod
-    def forward(ctx, x):
-        if get_world_size() > 1:
-            x = x.contiguous()
-            outputs = [torch.zeros_like(x) for _ in range(dist.get_world_size())]
-            dist.all_gather(outputs, x)
-            return torch.cat(outputs, 0)
-        return x
-
-    @staticmethod
-    def backward(_ctx, grads):
-        nworld = get_world_size()
-        if nworld > 1:
-            rank = get_rank()
-            s = (grads.shape[0] // nworld) * rank
-            e = (grads.shape[0] // nworld) * (rank + 1)
-            grads = grads.contiguous()
-            dist.all_reduce(grads)
-            return grads[s:e]
-        return grads
-
-
-class _AllReduceSum(torch.autograd.Function):
-
-    @staticmethod
-    def forward(_ctx, x: Tensor):
-        if get_world_size() > 1:
-            dist.all_reduce(x)
-        return x
-
-    @staticmethod
-    def backward(_ctx, grads):
-        return grads
-
-
-class _AllReduce(torch.autograd.Function):
-
-    @staticmethod
-    def forward(_ctx, x: Tensor):
-        nworld = get_world_size()
-        if nworld > 1:
-            x.div_(nworld)
-            dist.all_reduce(x)
-        return x
-
-    @staticmethod
-    def backward(_ctx, grads):
-        return grads
